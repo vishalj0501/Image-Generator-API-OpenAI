c6b7c3d7e69cc92794944e0ecaea175b65be914b
vishalj0501 <jvishal0501@gmail.com>
2023-04-16 15:56:17 +0530

    Add Req

--- prompts/generate.py
+++ prompts/generate.py
@@ -3,25 +3,45 @@ import sys
 import openai
 import json
 from dotenv import load_dotenv
-from model.model import Description
-from fastapi.response import Response
+# import models
+from models.model import Description
+# from fastapi.response import Response
+# from dataclasses import dataclass
 
 load_dotenv()
 # sys.path.append(sys)
 # print("-----\n")
 # print(sys.path[0])
 
+
+
+
 sys.path.append(sys.path.append(sys.path[0] + "/.."))
+# @dataclass
+# class Description():
+#     def __init__(self, prompt, n, squareSize):
+#         self.prompt = prompt
+#         self.n = n
+#         self.squareSize = squareSize
+
+# model=Description("This is a test", 1, "512x512")
 
 class ImageGenerator:
     def __init__(self):
         openai.api_key = os.getenv("API_KEY")
         self.APIKey=openai.api_key
 
-    def generateImage(self, description: Description):
-        prompt = description.prompt
-        n = description.n
-        squareSize = description.squareSize
+    def generateImage(self,description:Description):
+        openai.api_key = self.APIKey
+        response=openai.Image.create(
+        prompt = description.prompt,
+        n = description.n,
+        size = description.squareSize,
+        )
+        self.imageURL= response['data']
+        # print(self.imageURL)
+        self.imageURL=[image['url'] for image in self.imageURL]
+        # return response
         # response = openai.Completion.create(
         #     engine="davinci",
         #     prompt=prompt,
@@ -35,3 +55,5 @@ class ImageGenerator:
         # return response.choices[0].text
 
 
+generate=ImageGenerator()
+generate.generateImage()
--- router/router.py
+++ router/router.py
@@ -1,1 +1,32 @@
+from prompts.generate import ImageGenerator
+from fastapi import FastAPI, APIRouter
+from fastapi.responses import JSONResponse
 import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+sys.path.append(sys.path[0] + "/..")
+
+app = FastAPI()
+router = APIRouter()
+
+imageGen=ImageGenerator()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+router.add_api_route(
+    "api/generate",
+    endpoint=imageGen.generateImage,
+    methods=["POST"]
+)
+app.include_router(router)
+
+if __name__ == "__main__":
+    
